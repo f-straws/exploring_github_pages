98cace3ecdee9674dedb2a0568640abb0481deeb
F. Swart <f.swart@gmail.com>
2022-01-08 11:56:53 +0100

    publish pipeline with default values

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,13 @@
 # deploy.sh <your-name> <your-repo>
 GITHUB_USERNAME="${1}"
 GITHUB_REPO="${2}"
-GITHUB_PAGES_BRANCH="${3:?preview}"
-SOURCE_BRANCH="${4:?dev}"
+GITHUB_PAGES_BRANCH="${3:-preview}"
+SOURCE_BRANCH="${4:-dev}"
 
 cd docs/.vuepress/dist/${GITHUB_REPO} && \
 git init && \
 git add --all && \
-git commit --message 'deploy' #&& \
+git commit --message 'deploy'
+
 git push --force git@github.com:${GITHUB_USERNAME}/${GITHUB_REPO}.git ${SOURCE_BRANCH}:${GITHUB_PAGES_BRANCH}
 
