5695a429164e4123c8c64e86c5fcc5a956f95414
F. Swart <f.swart@gmail.com>
2022-01-08 11:41:32 +0100

    publish-pipeline

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+# deploy.sh <your-name> <your-repo>
+GITHUB_USERNAME="${1}"
+GITHUB_REPO="${2}"
+GITHUB_PAGES_BRANCH="${3:?preview}"
+SOURCE_BRANCH="${4:?dev}"
+
+cd docs/.vuepress/dist/${GITHUB_REPO} && \
+git init && \
+git add --all && \
+git commit --message 'deploy' #&& \
+git push --force git@github.com:${GITHUB_USERNAME}/${GITHUB_REPO}.git ${SOURCE_BRANCH}:${GITHUB_PAGES_BRANCH}
+
